1ed77835dc7301541c0844e6136dd52dee4ab59b
Pritam Sarkar <metaphorpritam@gmail.com>
2026-08-09 05:57:50 +0530

    Solve pandas-inspect-shape

--- study-plans/pandas-basics/pandas-inspect-shape/pandas-inspect-shape.py
+++ study-plans/pandas-basics/pandas-inspect-shape/pandas-inspect-shape.py
@@ -5,12 +5,17 @@ def inspect_dataframe(data):
     Returns: dict with 'rows', 'cols' (ints), 'columns' (list),
     'dtypes' (dict), 'total_values' (int)
     """
-    df = pd.DataFrame(data)
-    rows, cols = df.shape
+    df  = pd.DataFrame(data)
+    rows = df.shape[0]
+    cols = df.shape[1]
+    columns = df.columns.tolist()
+    dtypes = {col: str(dtypes) for col, dtypes in df.dtypes.items()}
+    total_values = df.size
+
     return {
         "rows": rows,
         "cols": cols,
-        "columns": df.columns.to_list(),
-        "dtypes": {col: str(df[col].dtype) for col in df.columns},
-        "total_values": rows * cols
+        "columns": columns,
+        "dtypes": dtypes,
+        "total_values": df.size
     }
