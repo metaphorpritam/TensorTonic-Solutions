67dc9d242666d7d4552e6d0c7f6a139dcfe293a7
Pritam Sarkar <metaphorpritam@gmail.com>
2026-08-27 14:55:50 +0530

    Solve sql-row-number

--- study-plans/sql-basics/sql-row-number/sql-row-number.sql
+++ study-plans/sql-basics/sql-row-number/sql-row-number.sql
@@ -1,8 +1,5 @@
 -- Write your SQL query here
-SELECT 
-    username
-    , segment
-    , engagement_score
-    , ROW_NUMBER() OVER (PARTITION BY segment ORDER BY engagement_score DESC) AS activity_rank
+SELECT username, segment, engagement_score,
+       ROW_NUMBER() OVER (PARTITION BY segment ORDER BY engagement_score DESC, username ASC) AS activity_rank
 FROM user_activity
 ORDER BY segment ASC, activity_rank ASC;
